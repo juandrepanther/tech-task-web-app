717fdff577e21c8228e0bd16370b9186f38accc5
juandrepanther <juandre@inbox.lv>
2024-05-16 18:44:36 +0300

    feat: add useWindowSize custom hook

--- src/components/appbar/appbar.tsx
+++ src/components/appbar/appbar.tsx
@@ -1,11 +1,14 @@
 import './appbar.scss'
+import { CiMenuFries } from 'react-icons/ci'
 
 export default function Appbar() {
   return (
     <div className="appbar">
       <div className="container page appbar__top">1</div>
       <div className="appbar__bottom">
-        <div className="container page">2</div>
+        <div className="container page">
+          2 <CiMenuFries size={32} />
+        </div>
       </div>
     </div>
   )
